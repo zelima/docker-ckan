73040e9181552bf6ded1fad2f0cbd5578774ea40
Petar Efnushev <petar.efnushev@keitaro.com>
2017-04-28 15:10:37 +0200

    Add base image for EDS

--- rootfs/setup/prerun.py
+++ rootfs/setup/prerun.py
@@ -4,13 +4,12 @@ import subprocess
 import psycopg2
 import urllib2
 
-
 ckan_ini = os.environ.get('CKAN_INI', '/srv/app/production.ini')
 
-RETRY = 5
+RETRY = 10
+
 
 def check_db_connection(retry=None):
-
     if retry is None:
         retry = RETRY
     elif retry == 0:
@@ -22,16 +21,17 @@ def check_db_connection(retry=None):
         connection = psycopg2.connect(conn_str)
 
     except psycopg2.Error as e:
-        print str(e)
         print '[prerun] Unable to connect to the database...try again in a while.'
         import time
         time.sleep(10)
-        check_db_connection(retry = retry - 1)
+        check_db_connection(retry=retry - 1)
+    except Exception as exc:
+        print 'Caught general exception: {0}'.format(exc)
     else:
         connection.close()
 
-def check_solr_connection(retry=None):
 
+def check_solr_connection(retry=None):
     if retry is None:
         retry = RETRY
     elif retry == 0:
@@ -40,47 +40,62 @@ def check_solr_connection(retry=None):
 
     url = os.environ.get('CKAN_SOLR_URL', '')
     search_url = '{url}/select/?q=*&wt=json'.format(url=url)
-
     try:
         connection = urllib2.urlopen(search_url)
     except urllib2.URLError as e:
+        print '[prerun] Unable to connect to solr [{0}]...try again in a while.'.format(search_url)
         print str(e)
-        print '[prerun] Unable to connect to solr...try again in a while.'
         import time
         time.sleep(10)
-        check_solr_connection(retry = retry - 1)
+        check_solr_connection(retry=retry - 1)
     else:
-        eval(connection.read())
+        _ = connection.read()
+        eval(_)
 
 
 def init_db():
+    db_command = ['paster', '--plugin=ckan', 'db', 'version', '-c', ckan_ini]
 
-    db_command = ['paster', '--plugin=ckan', 'db',
-                  'init', '-c', ckan_ini]
-    print '[prerun] Initializing or upgrading db - start'
+    # Setup datastore
+    ds_write = os.environ.get('CKAN__DATASTORE__WRITE_URL')
+    ds_read = os.environ.get('CKAN__DATASTORE__READ_URL')
+    cmd = ['paster', '--plugin=ckan', 'config-tool', ckan_ini, 'ckan.datastore.write_url = {0}'.format(ds_write)]
+    subprocess.call(cmd)
+
+    cmd = ['paster', '--plugin=ckan', 'config-tool', ckan_ini, 'ckan.datastore.read_url = {0}'.format(ds_read)]
+    subprocess.call(cmd)
+
+    print '[prerun] Checking db status - start'
     try:
         subprocess.check_output(db_command, stderr=subprocess.STDOUT)
-        print '[prerun] Initializing or upgrading db - end'
+        print '[prerun] Checking db status - end'
     except subprocess.CalledProcessError, e:
-        if 'OperationalError' in e.output:
-            print e.output
-            print '[prerun] Database not ready, waiting a bit before exit...'
-            import time
-            time.sleep(5)
-            sys.exit(1)
-        else:
-            print e.output
-            raise e
+        db_command = ['paster', '--plugin=ckan', 'db', 'init', '-c', ckan_ini]
+
+        print '[prerun] Initializing or upgrading db - start'
+        try:
+            subprocess.check_output(db_command, stderr=subprocess.STDOUT)
+            print '[prerun] Initializing or upgrading db - end'
+        except subprocess.CalledProcessError, e:
+            if 'OperationalError' in e.output:
+                print e.output
+                print '[prerun] Database not ready, waiting a bit before exit...'
+                import time
+                time.sleep(5)
+                sys.exit(1)
+            else:
+                print e.output
+                raise e
+    else:
+        print '[prerun] CKAN db is already initialized - skipping creation...'
 
 
 def create_sysadmin():
-
     name = os.environ.get('CKAN_SYSADMIN_NAME')
     password = os.environ.get('CKAN_SYSADMIN_PASSWORD')
     email = os.environ.get('CKAN_SYSADMIN_EMAIL')
 
     if name and password and email:
-
         # Check if user exists
         command = ['paster', '--plugin=ckan', 'user', name, '-c', ckan_ini]
 
@@ -90,28 +105,18 @@ def create_sysadmin():
             return
 
         # Create user
-        command = ['paster', '--plugin=ckan', 'user', 'add',
-                   name,
-                   'password=' + password,
-                   'email=' + email,
+        command = ['paster', '--plugin=ckan', 'user', 'add', name,
+                   'password={0}'.format(password),
+                   'email={0}'.format(email),
+                   'sysadmin=true',
                    '-c', ckan_ini]
 
         subprocess.call(command)
-        print '[prerun] Created user {0}'.format(name)
-
-        # Make it sysadmin
-        command = ['paster', '--plugin=ckan', 'sysadmin', 'add',
-                   name,
-                   '-c', ckan_ini]
-
-        subprocess.call(command)
-        print '[prerun] Made user {0} a sysadmin'.format(name)
+        print '[prerun] Created admin user {0}'.format(name)
 
 
 if __name__ == '__main__':
-
     maintenance = os.environ.get('MAINTENANCE_MODE', '').lower() == 'true'
-
     if maintenance:
         print '[prerun] Maintenance mode, skipping setup...'
     else:
