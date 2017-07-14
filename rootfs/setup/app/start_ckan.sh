8941f9862511738bf7369364918d9dd267209cfc
Marko Bocevski <marko.bocevski@keitaro.com>
2017-07-14 11:14:12 +0200

    Change from uwsgi to gunicorn

--- rootfs/setup/app/start_ckan.sh
+++ rootfs/setup/app/start_ckan.sh
@@ -2,10 +2,10 @@
 # Run the prerun script to init CKAN and create the default admin user
 python prerun.py
 
-# Set the common uwsgi options
-UWSGI_OPTS="--socket /tmp/uwsgi.sock --uid 92 --gid 92 --http :5000 --master --paste config:/srv/app/production.ini --gevent 50 -p 2 -L"
+# Set the common gunicorn options
+GUNICORN_OPTS="--log-file=- -k gevent -w 2 -b 0.0.0.0:5000 --paste production.ini"
 
-# Check whether http basic auth password protection is enabled and enable basicauth routing on uwsgi respecfully
+# Check whether http basic auth password protection is enabled and enable basicauth routing on gunicorn respecfully
 if [ $? -eq 0 ]
 then
   if [ "$PASSWORD_PROTECT" = true ]
@@ -14,15 +14,15 @@ then
     then
       # Generate htpasswd file for basicauth
       htpasswd -d -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
-      # Start uwsgi with basicauth
-      uwsgi --ini /srv/app/uwsgi.conf --pcre-jit $UWSGI_OPTS
+      # Start gunicorn with basicauth
+      gunicorn $GUNICORN_OPTS
     else
       echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
       exit 1
     fi
   else
-    # Start uwsgi
-    uwsgi $UWSGI_OPTS
+    # Start gunicorn
+    gunicorn $GUNICORN_OPTS
   fi
 else
   echo "[prerun] failed...not starting CKAN."
