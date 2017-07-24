57a5e16b49049a7e49a890a34f73c16fb5a0efe4
ilche <ilche.bedelovski@keitaro.info>
2017-07-24 14:41:30 +0200

    Adding HTTP Authentication in the Harvest image

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# Run the prerun script to init CKAN and create the default admin user
 python prerun.py
+
+# Set the common uwsgi options
+UWSGI_OPTS="--socket /tmp/uwsgi.sock --thunder-lock --uid 92 --gid 92 --http :5000 --master --single-interpreter --enable-threads --paste config:/srv/app/production.ini --gevent 2000 -p 4 -L"
+
 if [ $? -eq 0 ]
 then
+
   if [ "$HTTPS_REDIRECT" -eq "1" ]
   then
       cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
@@ -12,6 +18,25 @@ then
     supervisord --configuration /etc/supervisord.conf &
     gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
   fi
+  
+  # Check whether http basic auth password protection is enabled and enable basicauth routing on uwsgi respecfully
+  if [ "$PASSWORD_PROTECT" = true ]
+  then
+    if [ "$HTPASSWD_USER" ] || [ "$HTPASSWD_PASSWORD" ]
+    then
+      # Generate htpasswd file for basicauth
+      htpasswd -d -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
+      # Start uwsgi with basicauth
+      uwsgi --ini /srv/app/uwsgi.conf --pcre-jit $UWSGI_OPTS
+    else
+      echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
+      exit 1
+    fi
+  else
+    # Start uwsgi
+    uwsgi $UWSGI_OPTS
+  fi
+
 else
   echo "[prerun] failed...not starting CKAN."
 fi
