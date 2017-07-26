6a113fe476d6ddec7ae2b6b3d46d452fdfdb4c7c
Petar Efnushev <petar.efnushev@keitaro.com>
2017-07-26 16:34:15 +0200

    Increase gunicorn timeout & set log level to debug

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -9,13 +9,13 @@ then
       cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
       htpasswd -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
       nginx
-      gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+      gunicorn --log-file=- --log-level=DEBUG -k gevent -w 4 -b 127.0.0.1:4000 --timeout 90 --keep-alive 75 --capture-output --enable-stdio-inheritance --paste production.ini
     else
       echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
       exit 1
     fi
   else
-    gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
+    gunicorn --log-file=- --log-level=DEBUG -k gevent -w 4 -b 0.0.0.0:5000 --timeout 90 --keep-alive 75 --capture-output --enable-stdio-inheritance --paste production.ini
   fi
 else
   echo "[prerun] failed...not starting CKAN."
