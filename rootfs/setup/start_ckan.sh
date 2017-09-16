2a5395036f80bb4ef9363da7d1fc2582a8976f17
Zlatko Trajcheski <zlatko.trajcheski@keitaro.com>
2017-09-16 23:58:26 +0200

    Initial uswgi harvest redis

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
+# Run the prerun script to init CKAN and create the default admin user
 python prerun.py
+
+# Set the common uwsgi options
+UWSGI_OPTS="--socket /tmp/uwsgi.sock --uid 92 --gid 92 --http :5000 --master --enable-threads --paste config:/srv/app/production.ini --lazy-apps --gevent 2000 -p 4 -L"
+
+# Check whether http basic auth password protection is enabled and enable basicauth routing on uwsgi respecfully
 if [ $? -eq 0 ]
 then
-  if [ "$HTTPS_REDIRECT" -eq "1" ]
+  if [ "$PASSWORD_PROTECT" = true ]
   then
-      cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
-      nginx
+    if [ "$HTPASSWD_USER" ] || [ "$HTPASSWD_PASSWORD" ]
+    then
+      # Generate htpasswd file for basicauth
+      htpasswd -d -b -c /srv/app/.htpasswd $HTPASSWD_USER $HTPASSWD_PASSWORD
+      # Start supervisord
       supervisord --configuration /etc/supervisord.conf &
-      gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+      # Start uwsgi with basicauth
+      uwsgi --ini /srv/app/uwsgi.conf --pcre-jit $UWSGI_OPTS
+    else
+      echo "Missing HTPASSWD_USER or HTPASSWD_PASSWORD environment variables. Exiting..."
+      exit 1
+    fi
   else
+    # Start supervisord
     supervisord --configuration /etc/supervisord.conf &
-    gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
+    # Start uwsgi
+    uwsgi $UWSGI_OPTS
   fi
 else
   echo "[prerun] failed...not starting CKAN."
 fi
+
