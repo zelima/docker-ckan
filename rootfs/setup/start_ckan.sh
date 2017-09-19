72fc8828542b075bf4d801ef734bd92729c7f318
Zlatko Trajcheski <zlatko.trajcheski@keitaro.com>
2017-09-19 15:23:15 +0200

    Put 2 instead of 4 workers and harvest cron on 1 hour

--- rootfs/setup/start_ckan.sh
+++ rootfs/setup/start_ckan.sh
@@ -7,10 +7,10 @@ then
       cp -a /srv/app/nginx.conf /etc/nginx/nginx.conf
       nginx
       supervisord --configuration /etc/supervisord.conf &
-      gunicorn --log-file=- -k gevent -w 4 -b 127.0.0.1:4000 --paste production.ini
+      gunicorn --log-file=- -k gevent -w 2 -b 127.0.0.1:4000 --paste production.ini
   else
     supervisord --configuration /etc/supervisord.conf &
-    gunicorn --log-file=- -k gevent -w 4 -b 0.0.0.0:5000 --paste production.ini
+    gunicorn --log-file=- -k gevent -w 2 -b 0.0.0.0:5000 --paste production.ini
   fi
 else
   echo "[prerun] failed...not starting CKAN."
