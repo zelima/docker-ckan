1af5fb30c030bbcbb443eef4e2e3e379aeddb563
Vladimir Vojneski <vladimir.vojneski@gmail.com>
2018-11-27 15:46:15 +0100

    new branch for ckan 2.8.1

--- rootfs/setup/app/start_ckan.sh
+++ rootfs/setup/app/start_ckan.sh
@@ -3,9 +3,9 @@
 python prerun.py
 
 # Run any startup scripts provided by images extending this one
-if [[ -d "/docker-entrypoint.d" ]]
+if [[ -d "docker-entrypoint.d" ]]
 then
-    for f in /docker-entrypoint.d/*; do
+    for f in docker-entrypoint.d/*; do
         case "$f" in
             *.sh)     echo "$0: Running init file $f"; . "$f" ;;
             *.py)     echo "$0: Running init file $f"; python "$f"; echo ;;
